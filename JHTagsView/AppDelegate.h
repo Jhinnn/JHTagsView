5eb09e840c5ce700c35601cc194c4440f4c5e5ca
pengpengduduguagua <2979468570@qq.com>
2019-09-24 15:03:59 +0800

    Initial Commit

---
+++ JHTagsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHTagsView
+//
+//  Created by admin on 2019/9/24.
+//  Copyright © 2019 Jhinn1n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
